f5bbea74600bfd1697d7abd1f7a091d2cc448182
Levi Crews <20674880+levicrews@users.noreply.github.com>
2019-08-19 20:54:54 -0500

    remove unnecessary os package

--- .auto_init_commands.sh
+++ .auto_init_commands.sh
@@ -6,6 +6,8 @@ function project() {
     rm -rf $1/.git
     python3 create_project.py $1
     cd /home/levicrews/$1
+    sed '1,$d' README.md
+    echo "Repository for $1" >> README.md
     git init
     git remote add origin git@github.com:levicrews/$1.git
     git add .
@@ -19,6 +21,8 @@ function notebook() {
     rm -rf $1/.git
     python3 create_notebook.py $1
     cd /home/levicrews/$1
+    sed '1,$d' README.md
+    echo "This repository contains my notes on $1" >> README.md
     git init
     git remote add origin git@github.com:levicrews/$1.git
     git add .
--- create_project.py
+++ create_project.py
@@ -1,15 +1,11 @@
 import sys
-import os
 from github import Github
-
-path = "/home/levicrews/"
 
 username = "" #Insert your github username here
 password = "" #Insert your github password here
 
 def create_project():
     folderName = str(sys.argv[1])
-    os.makedirs(path + str(sys.argv[1]))
     user = Github(username, password).get_user()
     repo = user.create_repo(sys.argv[1])
     print("Succesfully created repository {}".format(sys.argv[1]))
