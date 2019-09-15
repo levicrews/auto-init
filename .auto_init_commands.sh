5130885f9d9a878ad82f2c7922c550c2202629b4
Levi Crews <20674880+levicrews@users.noreply.github.com>
2019-09-14 19:10:51 -0500

    Explicit file paths for clarity

--- .auto_init_commands.sh
+++ .auto_init_commands.sh
@@ -2,11 +2,11 @@
 
 function project() {
     cd ~
-    cp -a template-project $1
-    rm -rf $1/.git
+    cp -a ~/template-project ~/$1
+    rm -rf ~/$1/.git
     python3 create_project.py $1
-    cd $1
-    sed '1,$d' README.md
+    cd ~/$1
+    sed -i '1,$d' README.md
     echo "Repository for $1" >> README.md
     git init
     git remote add origin git@github.com:levicrews/$1.git
@@ -17,11 +17,11 @@ function project() {
 
 function notebook() {
     cd ~
-    cp -a template-notebook $1
-    rm -rf $1/.git
+    cp -a ~/template-notebook ~/$1
+    rm -rf ~/$1/.git
     python3 create_notebook.py $1
-    cd $1
-    sed '1,$d' README.md
+    cd ~/$1
+    sed -i '1,$d' README.md
     echo "This repository contains my notes on $1" >> README.md
     git init
     git remote add origin git@github.com:levicrews/$1.git
